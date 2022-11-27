0ba7ccd3d58e8265110dbfea5a01cb02142048f7
Mike <Genrih3256@gmail.com>
2022-11-28 01:43:34 +0200

    add second task flask

--- task2/hw2.py
+++ task2/hw2.py
@@ -15,12 +15,11 @@ def hello_world():
 @app.route("/password")
 def generate_password():
     symbols = string.ascii_letters + string.digits + string.punctuation
-    element = random.randrange(10, 20)
-    '''password'''
-    password = ''.join(random.choices(symbols, k=element))
-    print(password)
+    count_of_symbols_password = random.randrange(10, 20)
+    password = ''.join(random.choices(symbols, k=count_of_symbols_password ))
+
     return password
-generate_password()
+
 
 
 @app.route("/averege")
@@ -28,22 +27,21 @@ def calculate_average():
     with open('hw2.csv') as f:
         reader = csv.reader(f)
         head = next(reader)
-        '''get average height and get average weight'''
-        hights = []
-        counter = 0
-        weights = []
-        counterw = 1
-        for i in reader:
-            hight = (i[1])
-            weight = (i[2])
-            hights.append(float(hight))
-            weights.append(float(weight))
-            counter += 1
-            counterw += 1
+        list_hights = []
+        counter_element_high = 0
+        list_weights = []
+        counter_element_weight = 0
+        for row in reader:
+            hight = (row[1])
+            weight = (row[2])
+            list_hights.append(float(hight))
+            list_weights.append(float(weight))
+            counter_element_high += 1
+            counter_element_weight += 1
 
-    return f"<p>Average values of hight are {((sum(hights)))/(counter)} iches <br>\n" \
-           f" Average values of weight are {((sum(weights)))/(counterw)} pounds</p>"
-print(calculate_average())
+    return f"<p>Average values of hight are {((sum(list_hights)))/(counter_element_high)} iches <br>\n" \
+           f" Average values of weight are {((sum(list_weights)))/(counter_element_weight)} pounds</p>"
+
 
 
 app.run(port=5001)
